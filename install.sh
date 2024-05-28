d27f480ff1d9102124e7a6bb1774c1680983f0e2
zzucch <nikitamilovatskiy@gmail.com>
2024-05-28 15:54:55 +0300

    feat: update install script

--- install.sh
+++ install.sh
@@ -4,25 +4,32 @@ script_dir=$(dirname "$0")
 
 cd "$script_dir" || exit 1
 
-read -p "Do you want to update the dotfiles repository? (y/n) " -n 1 -r
+read -p "do you want to update the dotfiles repository? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	echo "Updating dotfiles..."
+	if ! command -v git &>/dev/null; then
+		echo "git could not be found, but is needed. install it and try again."
+		exit 1
+	fi
+
+	echo "updating dotfiles..."
 	git pull
 	git submodule update --remote
 fi
 
 if ! command -v stow &>/dev/null; then
-	echo "Stow could not be found, but is needed. Install it and try again."
+	echo "stow could not be found, but is needed. install it and try again."
 	exit 1
 fi
 
 find . -maxdepth 1 -not -name ".*" -type d |
 	while read -r dotfile; do
-		# Remove the leading "./"
+		# remove the leading "./"
 		dotfile=$(basename "$dotfile")
 
 		if ! stow --adopt -v "$dotfile"; then
-			echo -e "Failed to stow $dotfile"
+			echo -e "failed to stow $dotfile"
 		fi
 	done
+
+echo "done"
